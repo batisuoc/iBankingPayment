44ebcdbeab70ebd961d2e10368f78b5e01625ae6
HongLoc <hongloc2206@gmail.com>
2019-03-12 00:07:36 +0700

    css

--- php/login.php
+++ php/login.php
@@ -2,13 +2,26 @@
 	ob_start();
 	session_start();
 	?>
-
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 	<!DOCTYPE html>
 	<html>
 	<head>
 		<meta charset="UTF-8">
 		<title>Login</title>
+		<!--Made with love by Mutiullah Samim -->
+   
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+		<link rel="stylesheet" type="text/css" href="style.css">
+
 	</head>
 	<body>
 		<?php
@@ -42,22 +55,43 @@
 		}
 		?>
 		
-		<div class="login-page">
-			<div class="form-horizontal">   		
+		<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign In</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div>
+			</div>
+			<div class="card-body">
 				<form class="login-form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-					<p>
-						Username: <input type="text" placeholder="username" name="username" required autofocus/>
-					</p>
-
-					<p>
-						Password: <input type="password" placeholder="password" name="password" required/>
-					</p>
-					<input type="submit" name="login" value="Đăng nhập"></input>
-					<h4>
-						<?php echo $msg; ?>
-					</h4>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" placeholder="username" name="username" class="form-control" required autofocus/>
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" placeholder="password" name="password" class="form-control" required/>
+					</div>
+					
+					<div class="form-group">
+						<input type="submit" name="login" value="Đăng nhập" class="btn float-right login-btn"></input>
+					</div>
 				</form>
 			</div>
+			<div class="card-footer">
+				
+			</div>
 		</div>
+	</div>
+</div>
 	</body>
 	</html>
--- php/otpVerify.php
+++ php/otpVerify.php
@@ -1,14 +1,28 @@
 <?php
 	session_start();
 ?>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Verify OTP Code</title>
+		<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+
 	<?php
 		$resp="";
+		
 		if(isset($_POST['verify']) and !empty($_POST['otp']))
 		{
 			$ch = curl_init();
@@ -19,12 +33,42 @@
 			$resp = curl_exec($ch);
 			curl_close($ch);
 			echo $resp;
-		}	
+		}
+		
     ?>
 
-	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-		<input type="text" name="otp" placeholder="Enter your otp code">
-		<input type="submit" name="verify"/>
-	</form>
+
+	<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Verify otpcode</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div>
+			</div>
+			<div class="card-body">
+				<form class="login-form" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+						</div>
+						<input type="text" name="otp" placeholder="Enter your otp code" class="form-control" required autofocus/>
+						
+					</div>
+					
+					
+					<div class="form-group">
+						<input type="submit" name="verify" value="verify" class="btn float-right login-btn"></input>
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				
+			</div>
+		</div>
+	</div>
+</div>
 </body>
 </html>
